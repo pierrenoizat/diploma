68c313b9fed43d94a565b23d2a362ac189df6715
Pierre Noizat <noizat@hotmail.com>
2016-02-24 18:44:44 +0100

    CSRF bug fixed

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,8 +79,8 @@ Rails.application.configure do
   
   $ROOT_URL = "http://diploma.report"
   
-  config.middleware.use OmniAuth::Builder do
-    provider :google_oauth2, Rails.application.secrets.google_omniauth_key, Rails.application.secrets.google_omniauth_secret, {:skip_jwt => true, :scope => 'email,profile'}
-  end
+  # config.middleware.use OmniAuth::Builder do
+  #  provider :google_oauth2, Rails.application.secrets.google_omniauth_key, Rails.application.secrets.google_omniauth_secret, {:skip_jwt => true, :scope => 'email,profile'}
+  # end
   
 end
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
-# Rails.application.config.middleware.use OmniAuth::Builder do
-#  provider :google_oauth2, Rails.application.secrets.google_omniauth_key, Rails.application.secrets.google_omniauth_secret, {:skip_jwt => true, :scope => 'email,profile'}
-# end
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :google_oauth2, Rails.application.secrets.google_omniauth_key, Rails.application.secrets.google_omniauth_secret, {:skip_jwt => true, :scope => 'email,profile'}
+end
