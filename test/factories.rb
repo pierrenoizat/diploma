1c843e15c851f4d156db00a7118078fa9fcf511c
Pierre Noizat <noizat@hotmail.com>
2016-07-12 19:33:31 +0200

    Unit test added for issuer model

--- test/factories.rb
+++ test/factories.rb
@@ -2,6 +2,7 @@ FactoryGirl.define do
 factory :issuer do
   mpk "xpub661MyMwAqRbcG55r8ZsYPqywS8iK4endA313Q3xbSrn7gaJAjNM6yyviKLtfcp8KGLWRyPjh3utXYG6sqixndA6sbY6N4VEzPnJmR6ShMWG"
   name  "MySchool"
+  id '101'
 
   factory :issuer_with_batch do
     after(:create) do |issuer|
--- test/models/issuer_test.rb
+++ test/models/issuer_test.rb
@@ -1,5 +1,13 @@
 require 'test_helper'
 
 class IssuerTest < ActiveSupport::TestCase
+  test "should be findable by name" do
+    issuer= FactoryGirl.create(:issuer)
+    assert_equal issuer, Issuer.find_by_name("MySchool")
+  end
   
+  test "should get address from self.id and self.mpk" do
+    @issuer = FactoryGirl.create(:issuer)
+    assert_equal @issuer.payment_address, '1Ft77yhYz8ASd7Fgu42yak3jSiLRwD4nu5'
+  end
 end
